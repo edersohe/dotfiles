077d933e27238d76097053fdd8e66abcca608875
Eder Sosa <eder.sohe@gmail.com>
2025-08-17 06:20:32 -0600

    add doom emacs and qutebrowser

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,26 @@
+config.load_autoconfig(True)
+config.set("window.hide_decoration", True)
+config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
+config.set('content.cookies.accept', 'all', 'devtools://*')
+config.set('content.images', True, 'chrome-devtools://*')
+config.set('content.images', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome-devtools://*')
+config.set('content.javascript.enabled', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome://*/*')
+config.set('content.javascript.enabled', True, 'qute://*/*')
+config.set('colors.webpage.darkmode.enabled', True)
+c.tabs.position = 'top'
+c.url.default_page = 'https://duckduckgo.com/'
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'g': 'https://google.com/search?q={}'}
+c.url.start_pages = 'https://duckduckgo.com/'
+
+font = 'Hasklug Nerd Font'
+
+c.fonts.default_family = font
+c.fonts.default_size = "12pt"
+
+#c.zoom.default = 125
+
+config.source('nord-theme.py')
+config.set("colors.webpage.darkmode.enabled", True)
+
