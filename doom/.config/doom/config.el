0d7efeb9394f41c7d9b065e68aa58dd1f2475b54
Eder Sosa <eder.sohe@gmail.com>
2025-08-22 14:45:33 -0600

    style(doom): Reduce variable pitch font size from 20 to 19

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -22,7 +22,7 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "D2CodingLigature Nerd Font" :size 18 :weight 'semi-light)
-     doom-variable-pitch-font (font-spec :family "D2CodingLigature Nerd Font" :size 20))
+     doom-variable-pitch-font (font-spec :family "D2CodingLigature Nerd Font" :size 19))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
