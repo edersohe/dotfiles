c8a314b9b40862284094861ba81451edb5ebb01c
Eder Sosa <eder.sohe@gmail.com>
2025-08-22 22:16:15 -0600

    feat(emacs): Replace modal editing with completion-focused config
    - Remove Meow modal editing system and associated packages
    - Replace Vertico/Orderless/Corfu with built-in fido-vertical-mode
    - Switch from Catppuccin to built-in modus-vivendi-tinted theme
    - Remove doom-modeline in favor of default modeline
    - Add comprehensive completion configuration with flex matching
    - Enable completion-preview-mode for inline completions
    - Update font sizes and transparency settings

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -22,35 +22,50 @@
       frame-resize-pixelwise t
       load-prefer-newer t)
 
+(setq completion-styles '(basic flex)
+      completion-category-defaults nil
+      completion-category-overrides '((file (styles partial-completion)))
+      completion-auto-select 'second-tab
+      completion-auto-help 'always
+      completions-format 'one-column
+      completions-sort 'historical
+      completions-max-height 12
+      completion-ignore-case t
+      completions-detailed t
+      completion-show-help nil
+      tab-always-indent 'complete)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (hl-line-mode 1)
 
-(electric-pair-mode t)
 (save-place-mode t)
 (savehist-mode t)
 (recentf-mode t)
 (global-auto-revert-mode t)
+(fido-vertical-mode t)
 
 (require 'uniquify)
+
+(advice-add 'icomplete-post-command-hook :before #'minibuffer-hide-completions)
 
 (add-hook 'after-init-hook (lambda ()(setq gc-cons-threshold 800000)))
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'flymake-mode)
-
-;; Enable this when use a traditional Desktop Environment instead Tiling Window Manager
-;; (add-to-list 'initial-frame-alist '(fullscreen . fullboth))
-;; (add-to-list 'default-frame-alist '(fullscreen . fullboth))
+(add-hook 'prog-mode-hook #'completion-preview-mode)
+(add-hook 'prog-mode-hook #'electric-pair-mode)
 
 (display-time-mode 1)
 (setq display-time-format "%H:%M:%S")
 
-(set-face-attribute 'default nil :font "D2CodingLigature Nerd Font"
-		    :weight 'light ':height 140)
-(set-face-attribute 'fixed-pitch nil :font
-		    "D2CodingLigature Nerd Font"
-		    :height 160)
+(set-face-attribute 'default nil :font "D2CodingLigature Nerd Font" :weight 'semi-light ':height 135)
+(set-face-attribute 'fixed-pitch nil :font "D2CodingLigature Nerd Font" :height 150)
+
+(load-theme 'modus-vivendi-tinted)
+(add-to-list 'default-frame-alist '(alpha-background . 98))
+;; (add-to-list 'default-frame-alist '(fullscreen . fullboth))
+
 
 (defun my/kill-current-buffer ()
   "Kill the current buffer without prompting for its name."
@@ -92,54 +107,6 @@
   :config
   (exec-path-from-shell-initialize))
 
-(use-package catppuccin-theme
-  :ensure
-  t
-  :config
-  (load-theme 'catppuccin :no-confirm)
-  (add-to-list 'default-frame-alist '(alpha-background . 98)))
-
-(use-package doom-modeline
-  :ensure
-  t
-  :init
-  (doom-modeline-mode 1))
-
-(use-package orderless
-  :ensure
-  t
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides
-   '((file (styles basic partial-completion)))))
-
-(use-package vertico
-  :ensure
-  t
-  :after
-  orderless
-  :custom
-  (vertico-cycle t)
-  :config
-  (vertico-mode))
-
-(use-package marginalia
-  :ensure
-  t
-  :after
-  vertico
-  :config
-  (marginalia-mode))
-
-(use-package corfu
-  :ensure
-  t
-  :custom
-  (corfu-cycle t)
-  (corfu-auto t)
-  :init
-  (global-corfu-mode))
-
 (use-package treesit-auto
   :ensure
   t
@@ -171,7 +138,7 @@
       (gptel-send)
     (copilot-complete)))
 
-(global-set-key (kbd "C-<return>") 'smart-ai-complete)
+(global-set-key (kbd "M-RET") 'smart-ai-complete)
 
 (use-package copilot
   :ensure
@@ -185,7 +152,7 @@
   (:map copilot-completion-map
   ("C-n" . copilot-next-completion)
   ("C-p" . copilot-previous-completion)
-  ("<tab>" . copilot-accept-completione)))
+  ("TAB" . copilot-accept-completion)))
 
 (use-package gptel
   :ensure
@@ -205,104 +172,6 @@
   t
   :hook
   (eshell-load . eat-eshell-mode))
-
-(defun meow-setup ()
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("<escape>" . ignore)))
-
-(use-package meow
-  :ensure
-  t
-  :config
-  (meow-setup)
-  (meow-global-mode 1))
-
-(use-package meow-tree-sitter
-  :ensure
-  t
-  :after
-  meow
-  :config
-  (meow-tree-sitter-register-defaults))
 
 (provide 'init)
 ;;; init.el ends here
